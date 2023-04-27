e22d1fd50593d48fcbb8111826ea929aff00788b
Mac <mac@netrestore.local>
2023-04-27 11:57:45 +0200

    update_new3

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import render_template, send_from_directory
 import os 
 
 app = Flask(__name__)
-app.config['APPLICATION_ROOT'] = '/Gabz'
+app.config['APPLICATION_ROOT'] = '/gitSiteB3'
 FLATPAGES_EXTENSION = '.md'
 FLATPAGES_MARKDOWN_EXTENSIONS = ['extra']
 FLATPAGES_AUTO_RELOAD = True 
